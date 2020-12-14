f2cab9b3ea476f043a802408896360c31971d11f
Austin Ford <Austin.Ford@charter.com>
2020-12-14 00:17:45 -0700

    almost done

--- scripts/container.sh
+++ scripts/container.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
 NAME='elise'
-VERSION='latest'
+VERSION='testing'
 
 if [ "$1" == 'deploy' ]; then
     read -sp " password : " DOCKER_IMAGE_PASSWORD
     echo
+    docker build ${SHELL_ROOT_DIR} -t ${NAME}:${VERSION}
     docker run -dit \
         --name ${NAME} \
+        --env SHELL_ROOT_DIR='/root' \
         --env DOCKER_IMAGE_PASSWORD=${DOCKER_IMAGE_PASSWORD} \
         --volume "${SHELL_ROOT_DIR}":/root:rw \
-         jamesaustin87/${NAME}:${VERSION}
+         ${NAME}:${VERSION}
 
 elif [ "$1" == 'destroy' ]; then
     docker stop ${NAME}
     docker rm ${NAME}
-    docker rmi jamesaustin87/${NAME}:${VERSION}
+    docker rmi ${NAME}:${VERSION}
 
 elif [ "$1" == 'shell' ]; then
     docker exec -it ${NAME} /bin/bash
