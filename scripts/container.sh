b5bc9749e5f7f9a279d0217a9e997cc853935fab
Austin Ford <j.austin.ford@gmail.com>
2020-12-14 10:22:54 -0700

    automated debugging commit

--- scripts/container.sh
+++ scripts/container.sh
@@ -5,7 +5,10 @@ set -eu
 NAME='elise'
 VERSION='1.0'
 
-if [ "$1" == 'deploy' ]; then
+if [ "$1" == 'shell' ]; then
+    docker build ${SHELL_ROOT_DIR} -t ${NAME}:${VERSION}
+
+elif [ "$1" == 'deploy' ]; then
     docker build ${SHELL_ROOT_DIR} -t ${NAME}:${VERSION}
 
     docker run -dit \
@@ -16,9 +19,18 @@ if [ "$1" == 'deploy' ]; then
 
     docker exec -it ${NAME} /bin/bash
 
+elif [ "$1" == 'start' ]; then
+    docker start ${NAME}
+
+elif [ "$1" == 'stop' ]; then
+    docker stop ${NAME}
+
 elif [ "$1" == 'destroy' ]; then
     docker stop ${NAME}
     docker rm ${NAME}
     docker rmi ${NAME}:${VERSION}
+
+elif [ "$1" == 'shell' ]; then
+    docker exec -it ${NAME} /bin/bash
 
 fi
