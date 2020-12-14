1b0726281ce2b0656e3d29c9e8bddbe06bee6e50
Austin Ford <j.austin.ford@gmail.com>
2020-12-14 17:15:24 +0000

    roll all steps under deploy

--- scripts/container.sh
+++ scripts/container.sh
@@ -5,18 +5,18 @@ VERSION='1.0'
 
 if [ "$1" == 'deploy' ]; then
     docker build ${SHELL_ROOT_DIR} -t ${NAME}:${VERSION}
+
     docker run -dit \
         --name ${NAME} \
         --env SHELL_ROOT_DIR='/root' \
         --volume "${SHELL_ROOT_DIR}":/root:rw \
          ${NAME}:${VERSION}
 
+    docker exec -it ${NAME} /bin/bash
+
 elif [ "$1" == 'destroy' ]; then
     docker stop ${NAME}
     docker rm ${NAME}
     docker rmi ${NAME}:${VERSION}
-
-elif [ "$1" == 'shell' ]; then
-    docker exec -it ${NAME} /bin/bash
 
 fi
