064ec8039d73e799f13ec64430daa903c3232940
Austin Ford <j.austin.ford@gmail.com>
2020-12-19 00:25:10 -0700

    haproxy volume and tls settings

--- scripts/haproxy.sh
+++ scripts/haproxy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
+set -e
 
 . "${ELISE_ROOT_DIR}/src/elise.env"
 . "${ELISE_ROOT_DIR}/src/colors.sh"
@@ -9,21 +9,25 @@ set -eu
 
 dont_run_if_inside_docker
 
-if [ "$1" == "start" ]; then
+if [ "$1" == "deploy" ]; then
     ensure_root
     find_operating_system
-    install_docker_rpi
+    install_docker_centos
 
+    print_message 'stdout' 'decoding tls for volume'
+    echo "${LAB_NGINX_CERT}" | base64 -d > "${ELISE_ROOT_DIR}/haproxy/nginx.crt"
+    echo "${LAB_NGINX_KEY}" | base64 -d > "${ELISE_ROOT_DIR}/haproxy/nginx.crt.key"
+    
     print_message 'stdout' "deploying ${HAPROXY_NAME}" "${KUBE_MASTER_NODE_HOSTNAME}"
     docker run -dit \
         --name ${HAPROXY_NAME} \
         --publish 443:443 \
         --network bridge \
+        --volume ${ELISE_ROOT_DIR}/haproxy:/usr/local/etc/haproxy:ro \
         --restart unless-stopped \
-        --volume ${HAPROXY_VOLUME}:/usr/local/etc/haproxy:ro \
         haproxy:latest 1> /dev/null
 
-elif [ "$1" == "stop" ]; then
+elif [ "$1" == "destroy" ]; then
     print_message 'stdout' 'destroying haproxy' "${HAPROXY_NAME}"
     docker stop ${HAPROXY_NAME} 1> /dev/null
     docker rm ${HAPROXY_NAME} 1> /dev/null
