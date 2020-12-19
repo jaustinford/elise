e236103442a64cb37e234fc13245e2cc678172af
Austin Ford <j.austin.ford@gmail.com>
2020-12-19 14:19:45 -0700

    automated debugging commit

--- src/os.sh
+++ src/os.sh
@@ -16,19 +16,6 @@ disable_selinux () {
 install_docker () {
     if [ "$1" == 'Raspbian GNU/Linux 10 (buster)' ]; then
         if [ -z "$(dpkg --get-selections | grep docker-ce)" ]; then
-            if [ ! -f '/boot/cmdline_backup.txt' ]; then
-                print_message 'stdout' 'updating rpi cgroups'
-                cp /boot/cmdline.txt /boot/cmdline_backup.txt
-                orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
-                echo ${orig} | tee /boot/cmdline.txt
-
-                print_message 'stdout' 'rebooting host' "$(hostname)"
-                reboot
-
-            fi
-
-            apt-get update -y
-            apt-get upgrade -y
             print_message 'stdout' 'installing docker on' "$1"
             curl -sSL https://get.docker.com | sh
 
@@ -37,8 +24,6 @@ install_docker () {
     elif [ "$1" == 'CentOS Linux 8' ]; then
         if [ -z "$(rpm -qa | grep docker-ce)" ]; then
             print_message 'stdout' 'installing docker on' "$1"
-            yum update -y
-            yum upgrade -y
             dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
             dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm -y
             dnf install docker-ce -y
@@ -109,7 +94,19 @@ turn_swap_off () {
 
 prepare_master_node () {
     print_message 'stdout' 'preparing master' "$(hostname)"
-    if [ "$1" == 'CentOS Linux 8' ]; then
+    if [ "$1" == 'Raspbian GNU/Linux 10 (buster)' ]; then
+        if [ ! -f '/boot/cmdline_backup.txt' ]; then
+            print_message 'stdout' 'updating rpi cgroups'
+            cp /boot/cmdline.txt /boot/cmdline_backup.txt
+            orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
+            echo ${orig} | tee /boot/cmdline.txt
+
+            print_message 'stdout' 'rebooting host' "$(hostname)"
+            reboot
+
+        fi
+
+    elif [ "$1" == 'CentOS Linux 8' ]; then
         disable_selinux "$operating_system"
         print_message 'stdout' 'adding k8s firewalld'
         firewall-cmd --permanent --add-port=6443/tcp 1> /dev/null
