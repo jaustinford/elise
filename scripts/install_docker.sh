01643ad11beaaf29f6ee23b27221b08627589b00
Austin Ford <j.austin.ford@gmail.com>
2020-12-19 12:24:36 -0700

    automated debugging commit

---
+++ scripts/install_docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+. "${ELISE_ROOT_DIR}/src/elise.env"  
+. "${ELISE_ROOT_DIR}/src/colors.sh"
+. "${ELISE_ROOT_DIR}/src/general.sh"
+. "${ELISE_ROOT_DIR}/src/os.sh"
+
+ensure_root
+dont_run_if_inside_docker
+find_operating_system
+install_docker "$operating_system"
