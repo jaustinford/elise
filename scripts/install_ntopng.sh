c22047bf16b956f886c1322d822bcc69dc75f14d
Austin Ford <j.austin.ford@gmail.com>
2020-12-19 20:38:48 -0700

    automated debugging commit

--- scripts/install_ntopng.sh
+++ scripts/install_ntopng.sh
@@ -8,12 +8,6 @@ set -e
 . "${ELISE_ROOT_DIR}/src/ntopng.sh"
 . "${ELISE_ROOT_DIR}/src/os.sh"
 
-if [ "$(hostname)" != 'netmon.labs.elysianskies.com' ]; then
-    print_message 'stderr' 'host match failed!'
-    exit 1
-
-fi
-
 ensure root
 find_operating_system
 install_ntopng "$operating_system" "${NTOPNG_REPO}"
