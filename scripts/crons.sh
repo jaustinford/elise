c59973aa6d65f0aaa525bf8d77109d84ab8a4cf4
Austin Ford <j.austin.ford@gmail.com>
2020-12-20 19:06:39 -0700

    automated debugging commit

--- scripts/crons.sh
+++ scripts/crons.sh
@@ -14,7 +14,7 @@ install_cron "$operating_system"
 
 if [ "$(hostname)" == "${ISCSI_CRON_HOST}" ]; then
     print_message 'stdout' 'copy cron jobs' "$(hostname)"
-    cp "${ELISE_ROOT_DIR}/crons/escrons" /etc/cron.d/escrons
+    cp "${ELISE_ROOT_DIR}/files/escrons" /etc/cron.d/escrons
     chmod 644 /etc/cron.d/escrons
     
     print_message 'stdout' "reloading $cron_service"
