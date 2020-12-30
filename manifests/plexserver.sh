f308b9793f947fd403e756527d01a3aa24da27d1
Austin Ford <j.austin.ford@gmail.com>
2020-12-29 19:30:32 -0700

    simplify plexserver deployment

--- manifests/plexserver.sh
+++ manifests/plexserver.sh
@@ -47,56 +47,13 @@ spec:
           value: "${DOCKER_TIMEZONE}"
         - name: PLEX_CLAIM
           value: "${PLEX_CLAIM}"
-        ports:
-        - containerPort: 32400
-          protocol: TCP
-        - containerPort: 3005
-          protocol: TCP
-        - containerPort: 8324
-          protocol: TCP
-        - containerPort: 32469
-          protocol: TCP
-        - containerPort: 1900
-          protocol: UDP
-        - containerPort: 32410
-          protocol: UDP
-        - containerPort: 32412
-          protocol: UDP
-        - containerPort: 32413
-          protocol: UDP
-        - containerPort: 32414
-          protocol: UDP
         volumeMounts:
         - name: k8s-vol-plexserver-config
           mountPath: /config/Library/Application Support
         - name: k8s-vol-plexserver-transcode
           mountPath: /transcode
-        - name: k8s-vol-plexserver-content-standup
-          mountPath: /shares/tvault/video/standup
-          readOnly: true
-        - name: k8s-vol-plexserver-content-dj-recordings
-          mountPath: /shares/tvault/video/dj-recordings
-          readOnly: true
-        - name: k8s-vol-plexserver-content-documentaries
-          mountPath: /shares/tvault/video/documentaries
-          readOnly: true
-        - name: k8s-vol-plexserver-content-movies
-          mountPath: /shares/tvault/video/movies
-          readOnly: true
-        - name: k8s-vol-plexserver-content-usa-movies
-          mountPath: /shares/tvault/video/usa-movies
-          readOnly: true
-        - name: k8s-vol-plexserver-content-usa-tv
-          mountPath: /shares/tvault/video/usa-tv
-          readOnly: true
-        - name: k8s-vol-plexserver-content-tv
-          mountPath: /shares/tvault/video/tv
-          readOnly: true
-        - name: k8s-vol-plexserver-content-anime-movies
-          mountPath: /shares/tvault/video/anime-movies
-          readOnly: true
-        - name: k8s-vol-plexserver-content-anime-tv
-          mountPath: /shares/tvault/video/anime-tv
+        - name: k8s-vol-plexserver-content
+          mountPath: /shares/tvault/video
           readOnly: true
       volumes:
       - name: k8s-vol-plexserver-config
@@ -121,33 +78,9 @@ spec:
           chapAuthSession: true
           secretRef:
              name: tvault-iscsi-chap
-      - name: k8s-vol-plexserver-content-standup
+      - name: k8s-vol-plexserver-content
         hostPath:
-          path: /mnt/tvault/video/standup
-      - name: k8s-vol-plexserver-content-dj-recordings
-        hostPath:
-          path: /mnt/tvault/video/dj-recordings
-      - name: k8s-vol-plexserver-content-documentaries
-        hostPath:
-          path: /mnt/tvault/video/documentaries
-      - name: k8s-vol-plexserver-content-movies
-        hostPath:
-          path: /mnt/tvault/video/movies
-      - name: k8s-vol-plexserver-content-usa-movies
-        hostPath:
-          path: /mnt/tvault/video/usa-movies
-      - name: k8s-vol-plexserver-content-usa-tv
-        hostPath:
-          path: /mnt/tvault/video/usa-tv
-      - name: k8s-vol-plexserver-content-tv
-        hostPath:
-          path: /mnt/tvault/video/tv
-      - name: k8s-vol-plexserver-content-anime-movies
-        hostPath:
-          path: /mnt/tvault/video/anime-movies
-      - name: k8s-vol-plexserver-content-anime-tv
-        hostPath:
-          path: /mnt/tvault/video/anime-tv
+          path: /mnt/tvault/video
       nodeSelector:
           kubernetes.io/hostname: ${PLEX_AFFINITY_NODE}
 EOF
