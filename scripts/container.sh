4e586f92e413b3b1a89f9b81d5b57429a8cdd4c8
Austin Ford <j.austin.ford@gmail.com>
2021-03-13 14:58:33 -0700

    migrate container script to docker hub

--- scripts/container.sh
+++ scripts/container.sh
@@ -2,54 +2,50 @@
 
 . "${ELISE_ROOT_DIR}/src/elise.sh"
 
-NAME='elise'
-VERSION='1.0'
+CONTAINER_NAME='elise'
+IMAGE_NAME='jamesaustin87/es-centos'
+IMAGE_VERSION='1.0'
 
 if [ "${MSYSTEM}" == 'MINGW64' ]; then
     ROOT_MOUNT_PATH='//root'
     ROOT_VOLUME="/${ELISE_ROOT_DIR}://root"
-    SHELL_CMD="winpty docker exec -it ${NAME} //bin/bash"
+    SHELL_CMD="winpty docker exec -it ${CONTAINER_NAME} //bin/bash"
 
 else
     ROOT_MOUNT_PATH='/root'
     ROOT_VOLUME="${ELISE_ROOT_DIR}:/root"
-    SHELL_CMD="docker exec -it ${NAME} /bin/bash"
+    SHELL_CMD="docker exec -it ${CONTAINER_NAME} /bin/bash"
 
 fi
 
-if [ "$1" == 'build' ]; then
-    docker build ${ELISE_ROOT_DIR} -t ${NAME}:${VERSION}
-
-elif [ "$1" == 'deploy' ]; then
-    docker build ${ELISE_ROOT_DIR} -t ${NAME}:${VERSION}
-
+if [ "$1" == 'deploy' ]; then
     docker run -dit \
-        --name ${NAME} \
+        --name ${CONTAINER_NAME} \
         --network=host \
         --privileged \
         --env TZ=${DOCKER_TIMEZONE} \
         --env HOST_HOSTNAME="$(hostname)" \
         --env ELISE_ROOT_DIR=${ROOT_MOUNT_PATH} \
         --volume ${ROOT_VOLUME}:rw \
-        ${NAME}:${VERSION}
+        ${IMAGE_NAME}:${IMAGE_VERSION}
 
     ${SHELL_CMD}
 
 elif [ "$1" == 'start' ]; then
-    docker start ${NAME}
+    docker start ${CONTAINER_NAME}
     ${SHELL_CMD}
 
 elif [ "$1" == 'stop' ]; then
-    docker stop ${NAME}
+    docker stop ${CONTAINER_NAME}
 
 elif [ "$1" == 'remove' ]; then
-    docker stop ${NAME}
-    docker rm ${NAME}
+    docker stop ${CONTAINER_NAME}
+    docker rm ${CONTAINER_NAME}
 
 elif [ "$1" == 'destroy' ]; then
-    docker stop ${NAME}
-    docker rm ${NAME}
-    docker rmi ${NAME}:${VERSION}
+    docker stop ${CONTAINER_NAME}
+    docker rm ${CONTAINER_NAME}
+    docker rmi ${IMAGE_NAME}:${IMAGE_NAME}
 
 elif [ "$1" == 'shell' ]; then
     ${SHELL_CMD}
