107c0dc637607df22de49d8516a505ec45d3a95a
Austin Ford <Austin.Ford@charter.com>
2021-03-13 21:47:16 -0700

    configure entrypoint for vault file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-if [ ! -f "${ELISE_ROOT_DIR}/src/elise.sh" ]; then
+if [ ! -f "${ELISE_ROOT_DIR}/.vault.txt" ]; then
+    echo "missing ansible-vault password file"
     exit 1
 
 fi
