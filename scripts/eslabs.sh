ec64eef7023a3b9f8a6f7cf831c8464b714d1949
Austin Ford <j.austin.ford@gmail.com>
2021-03-14 12:03:38 -0600

    automated debugging commit

--- scripts/eslabs.sh
+++ scripts/eslabs.sh
@@ -17,7 +17,7 @@ if [ "${OPTION}" == 'destroy' ]; then
 
     print_message 'stdout' 'stopping all resources'
     ${ELISE_ROOT_DIR}/scripts/kube_automator.sh stop eslabs all
-    ansible-playbook ${ELISE_ROOT_DIR}/ansible/playbooks/watchers --extra-vars "option=stop"
+    ansible-playbook ${ELISE_ROOT_DIR}/ansible/playbooks/watchers.yml --extra-vars "option=stop"
 
     print_message 'stdout' 'destroying cluster'
     ansible-playbook ${ELISE_ROOT_DIR}/ansible/playbooks/k8s_cluster_destroy.yml
@@ -59,7 +59,7 @@ elif [ "${OPTION}" == 'deploy' ]; then
     print_message 'stdout' 'deploying all other eslabs resources'
     ${ELISE_ROOT_DIR}/manifests/plex.sh apply; ${ELISE_ROOT_DIR}/scripts/kube_automator.sh start eslabs plex
     ${ELISE_ROOT_DIR}/manifests/kharon.sh apply; ${ELISE_ROOT_DIR}/scripts/kube_automator.sh start eslabs kharon
-    ansible-playbook ${ELISE_ROOT_DIR}/ansible/playbooks/watchers --extra-vars "option=start"
+    ansible-playbook ${ELISE_ROOT_DIR}/ansible/playbooks/watchers.yml --extra-vars "option=start"
     ${ELISE_ROOT_DIR}/manifests/bigbrother.sh apply; ${ELISE_ROOT_DIR}/scripts/kube_automator.sh start eslabs bigbrother
     ${ELISE_ROOT_DIR}/manifests/filebrowser.sh apply; ${ELISE_ROOT_DIR}/scripts/kube_automator.sh start eslabs filebrowser
     ansible-playbook ${ELISE_ROOT_DIR}/ansible/playbooks/nrpe.yml
