ae5584c5e4e028f67c2d67c8145d16537854f3f7
Austin Ford <Austin.Ford@charter.com>
2021-03-13 21:59:41 -0700

    newton

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -f "${ELISE_ROOT_DIR}/.vault.txt" ]; then
-    echo "missing ansible-vault password file"
+    echo "ah, ah, ah, you didn't say the magic word..."
     exit 1
 
 fi
