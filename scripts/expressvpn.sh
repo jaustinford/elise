d8ab5b8b7e15cfdf86ad48181533ba58a2930131
Austin Ford <j.austin.ford@gmail.com>
2021-03-30 23:36:01 -0600

    expand more on expressvpn scripting

--- scripts/expressvpn.sh
+++ scripts/expressvpn.sh
@@ -7,26 +7,25 @@ set -e
 . "${ELISE_ROOT_DIR}/src/kubernetes.sh"
 
 MODE="$1"
+SERVER="$2"
 
 if [ "${MODE}" == 'change' ]; then
-    print_message stdout 'new expressvpn server' "${KHARON_EXPRESSVPN_SERVER}"
-    if [ ! -z "$(pod_from_deployment eslabs kharon)" ]; then
-        grab_loaded_vpn_server "$(pod_from_deployment eslabs kharon)"
+    if [ -z "${SERVER}" ]; then SERVER="${KHARON_EXPRESSVPN_SERVER}"
+    fi
 
-        if [ "$loaded_vpn_server" != "${KHARON_EXPRESSVPN_SERVER}" ]; then
-            "${ELISE_ROOT_DIR}/scripts/kube_automator.sh" stop eslabs kharon
-            "${ELISE_ROOT_DIR}/scripts/run_manifest.sh" apply kharon 1> /dev/null
-            "${ELISE_ROOT_DIR}/scripts/kube_automator.sh" start eslabs kharon
+    grab_loaded_vpn_server "$(pod_from_deployment eslabs kharon)"
 
-        else
-            print_message stderr 'server already connected!' "${KHARON_EXPRESSVPN_SERVER}"
-            exit 1
-
-        fi
-
-    else
+    if [ "$loaded_vpn_server" != "${SERVER}" ]; then
+        sed -i -E "s/^KHARON_EXPRESSVPN_SERVER.*$/KHARON_EXPRESSVPN_SERVER='${SERVER}'/g" "${ELISE_ROOT_DIR}/src/elise.sh"
+        . "${ELISE_ROOT_DIR}/src/elise.sh"
+        "${ELISE_ROOT_DIR}/scripts/kube_automator.sh" stop eslabs kharon
         "${ELISE_ROOT_DIR}/scripts/run_manifest.sh" apply kharon 1> /dev/null
         "${ELISE_ROOT_DIR}/scripts/kube_automator.sh" start eslabs kharon
+
+
+    else
+        print_message stderr 'server already connected!' "${SERVER}"
+        exit 1
 
     fi
 
