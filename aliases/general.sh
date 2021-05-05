4bc739bc27141fbbdda36d81978fc3dc8f4716d0
Austin Ford <j.austin.ford@gmail.com>
2021-05-05 08:04:50 -0600

    update aliases

--- aliases/general.sh
+++ aliases/general.sh
@@ -12,8 +12,10 @@ alias eslabs_deploy="${ELISE_ROOT_DIR}/scripts/eslabs.sh deploy"
 alias eslabs_destroy="${ELISE_ROOT_DIR}/scripts/eslabs.sh destroy"
 alias hyperion_off="${KUBE_AUTOMATOR} stop eslabs hyperion; ${ANSIBLE_PLAYBOOK}/watchers.yml --extra-vars \"option=stop\""
 alias hyperion_on="${KUBE_AUTOMATOR} start eslabs hyperion; ${ANSIBLE_PLAYBOOK}/watchers.yml --extra-vars \"option=start\""
-alias logs_haproxy='ssh kube00 sudo docker logs -f haproxy'
-alias logs_soteria="ssh kube02 tail -f ${LOG_DIR}/soteria-kube02.labs.elysianskies.com.log"
+alias logs_full_haproxy='ssh kube00 sudo docker logs haproxy'
+alias logs_full_soteria="ssh kube02 cat ${LOG_DIR}/soteria-kube02.labs.elysianskies.com.log"
+alias logs_tail_haproxy='ssh kube00 sudo docker logs -f haproxy'
+alias logs_tail_soteria="ssh kube02 tail -f ${LOG_DIR}/soteria-kube02.labs.elysianskies.com.log"
 alias nuke="countdown_to_cmd \"${ELISE_ROOT_DIR}/scripts/eslabs.sh destroy; ${ELISE_ROOT_DIR}/scripts/eslabs.sh deploy\""
 alias pihole_update='ssh dns sudo pihole -up'
 alias scp='scp -F /tmp/ssh_config -i /tmp/id_rsa -o StrictHostKeyChecking=no -o LogLevel=ERROR'
